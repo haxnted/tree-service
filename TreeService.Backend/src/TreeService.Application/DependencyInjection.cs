54f86bc70c6f4b949ed6c824e72f37febf5f39cb
Alexandr <bagnencko.sasha1@gmail.com>
2024-11-21 11:07:22 +0300

    update summery di

--- TreeService.Backend/src/TreeService.Application/DependencyInjection.cs
+++ TreeService.Backend/src/TreeService.Application/DependencyInjection.cs
@@ -4,13 +4,27 @@ using TreeService.Application.Abstractions;
 
 namespace TreeService.Application;
 
+/// <summary>
+///     Методы расширения для регистрации Application Layer
+/// </summary>
 public static class DependencyInjection
 {
+    /// <summary>
+    ///     Регистрация Application Layer
+    /// </summary>
+    /// <param name="services">Коллекция сервисов</param>
+    /// <returns>Коллекция сервисов</returns>
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         return services.AddHandlers()
             .AddValidatorsFromAssembly(typeof(DependencyInjection).Assembly);
     }
+
+    /// <summary>
+    ///     Регистрация сервисов handlers с помощью библиотеки Scrutor
+    /// </summary>
+    /// <param name="collection">Коллекция сервисов</param>
+    /// <returns>Коллекция сервисов</returns>
     private static IServiceCollection AddHandlers(this IServiceCollection collection)
     {
         collection.Scan(scan => scan.FromAssemblies(typeof(DependencyInjection).Assembly)
